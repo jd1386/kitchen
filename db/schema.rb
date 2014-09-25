30136951817699333773ac0ea6b69fcb3933725a
Jungdo Lee <lee.jungdo@gmail.com>
2014-09-25 14:28:09 +0900

    add comment associated with recipe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925044007) do
+ActiveRecord::Schema.define(version: 20140925052657) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "recipe_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["recipe_id"], name: "index_comments_on_recipe_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "food_items", force: true do |t|
     t.string   "name"
