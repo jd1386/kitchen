7b2668eebbd7aef182b5da4e74811a16a637a229
Jungdo Lee <lee.jungdo@gmail.com>
2014-09-26 22:55:03 +0900

    add full_name_of(user) method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
     /^http/i.match(url) ? url : "http://#{url}"
   end
 
+  def full_name_of(user)
+  	user.first_name + ' ' + user.last_name unless user.first_name.blank? && user.last_name.blank?
+  end
+
 end
